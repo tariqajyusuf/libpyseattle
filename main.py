a18f6d6ad77222d1d76c9edcda3205d5cb65f40d
Tariq Yusuf <tariq@tariqyusuf.in>
2023-04-17 12:42:26 -0700

    Add date search to narrow down daily usage.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+from datetime import date
 from getpass import getpass
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -32,10 +34,21 @@ WebDriverWait(driver, timeout=10).until(lambda driver : driver.find_element(by=B
 driver.find_element(by=By.XPATH, value="//button[text() = 'Daily']").click()
 logging.info("Loaded page - %s" % driver.title)
 
-WebDriverWait(driver, timeout=10).until(lambda driver : driver.find_element(by=By.XPATH, value="//input[@placeholder = 'End Date']"))
+WebDriverWait(driver, timeout=10).until(lambda driver : driver.find_element(by=By.CLASS_NAME, value="fusioncharts-container"))
 end_date = driver.find_element(by=By.XPATH, value="//input[@placeholder = 'End Date']")
+start_date = driver.find_element(by=By.XPATH, value="//input[@placeholder = 'Start Date']")
 logging.info("Loaded page - %s" % driver.title)
-print(end_date.get_attribute("value"))
+logging.info("Getting energy usage from the last 30 days")
+
+# The city only provides data on a daily basis, so today's energy won't be available.
+old_value = end_date.get_attribute("value")
+end_date.clear()
+end_date.send_keys((date.today() - datetime.timedelta(days=1)).strftime("%m-%d-%Y"))
+start_date.clear()
+start_date.send_keys((date.today() - datetime.timedelta(days=30)).strftime("%m-%d-%Y"))
+WebDriverWait(driver, timeout=10).until(lambda driver : end_date.get_attribute("value") != old_value)
+driver.find_element(by=By.XPATH, value="//button[text() = 'Update']").click()
+WebDriverWait(driver, timeout=10).until(lambda driver : driver.find_element(by=By.XPATH, value="fusioncharts-container"))
 
 """text_box = driver.find_element(by=By.NAME, value="my-text")
 submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
